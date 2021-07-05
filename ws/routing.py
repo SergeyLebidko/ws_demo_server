662759e368777746ddf05bc39825a2d5e162e5b7
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-05 18:58:59 +0300

    Исправлен адрес первого сокет-маршрута

--- ws/routing.py
+++ ws/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .consumers import EchoConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/tick_tock/$', EchoConsumer.as_asgi())
+    re_path(r'ws/echo/$', EchoConsumer.as_asgi())
 ]
