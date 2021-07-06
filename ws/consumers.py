a9bc90f0245a716db00f8b19649d66a294d01dd5
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-06 18:30:52 +0300

    Правка EchoConsumer

--- ws/consumers.py
+++ ws/consumers.py
@@ -1,11 +1,19 @@
 from channels.generic.websocket import WebsocketConsumer
+from channels.exceptions import StopConsumer
 
 
 class EchoConsumer(WebsocketConsumer):
 
+    def __init__(self):
+        self.count = 0
+        WebsocketConsumer.__init__(self)
+
     def connect(self):
-        print('Connect')
         self.accept()
 
+    def receive(self, text_data=None, bytes_data=None):
+        self.count += 1
+        self.send(f'echo_{self.count}')
+
     def disconnect(self, code):
-        print('Disconnect with code', code)
+        raise StopConsumer
