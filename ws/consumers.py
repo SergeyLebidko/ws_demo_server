a0a4d746f400f3c1bc315ae50d2f914fcbf76932
SergeyLebidko <it.sergeyler@mail.ru>
2021-07-07 16:30:50 +0300

    Добавлено GroupEchoConsumer

--- ws/consumers.py
+++ ws/consumers.py
@@ -1,4 +1,4 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 from channels.exceptions import StopConsumer
 
 
@@ -16,4 +16,30 @@ class EchoConsumer(WebsocketConsumer):
         self.send(f'echo_{self.count}')
 
     def disconnect(self, code):
+        raise StopConsumer
+
+
+class GroupEchoConsumer(AsyncWebsocketConsumer):
+    groups = ['broadcast']
+
+    async def connect(self):
+        await self.accept()
+        print('Соединение установлено')
+
+    async def receive(self, text_data=None, bytes_data=None):
+        print('Получено сообщение:', text_data)
+        await self.channel_layer.group_send(
+            'broadcast',
+            {
+                'type': 'group.message',
+                'message': text_data
+            }
+        )
+
+    async def group_message(self, event):
+        print(f'Рассылаю сообщение "{event["message"]}" членам группы')
+        await self.send(event['message'])
+
+    async def disconnect(self, code):
+        print('Соединение разорвано. Код:', code)
         raise StopConsumer
--- ws/routing.py
+++ ws/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 
-from .consumers import EchoConsumer
+from .consumers import EchoConsumer, GroupEchoConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/echo/$', EchoConsumer.as_asgi())
+    re_path(r'ws/echo/$', EchoConsumer.as_asgi()),
+    re_path(r'ws/group_echo/$', GroupEchoConsumer.as_asgi())
 ]
